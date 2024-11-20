8cc17ab0e28c2acf8c4d19fd1242aedb128b922e
Diego Caldas Salengue <diego.salengue@lifemed.com.br>
2024-11-19 21:37:24 -0300

    Commit inicial.

---
+++ projeto-inicial.php
@@ -0,0 +1,13 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$student = new Student(
+    null,
+    'Vinicius Dias',
+    new \DateTimeImmutable('1976-03-05')
+);
+
+echo $student->age();
